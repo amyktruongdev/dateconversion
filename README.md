c84ad543ebb59269de6160fae992fe11be033937
Amy Truong <amyktruongdev@gmail.com>
2024-09-19 20:12:57 -0700

    trying to import to excel

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # dateconversion
+
+install pandas and selenium-webmanager and openpyxl
+trying to make the data show up in excel
--- events.py
+++ events.py
@@ -1,8 +1,9 @@
-import pandas
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
+import pandas as pd
 
 
 # Set up ChromeDriver using webdriver-manager to handle the driver installation
@@ -19,12 +20,23 @@ page_source = driver.page_source
 soup = BeautifulSoup(page_source, 'html.parser')
 
 # Step 4: Find all span tags with class 'tribe-event-date-start'
-span_tags = soup.find_all('span', class_='tribe-event-date-start')
-end_time = soup.find_all('span', class_='tribe-event-time')
+span_tags = soup.find_all('span', class_='tribe-event-date-start tribe-event-time')
+#end_time = soup.find_all('span', class_='tribe-event-time')
+
+data = []
+for span in span_tags:
+    
+    item = {}
+    item['Start Date'] = span.find("span",class_="tribe-event-date-start").text
+
+    data.append(item)
 
 # Step 5: Print the content of each found span tag
-for span, meow in zip(span_tags, end_time):
-    print(span.text + " to " + meow.text)
+#for span, meow in zip(span_tags, end_time):
+    #print(span.text + " to " + meow.text)
 
 # Step 6: Close the browser
 driver.quit()
+
+df = pd.DataFrame(data)
+df.to_excel("dates.xlsx")
